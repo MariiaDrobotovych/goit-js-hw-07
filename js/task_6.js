09ef0d45b0d753a5bc5f8187146c581c4087bd17
Maria <maria@phyxologs-MacBook-Pro.local>
2021-02-20 23:29:26 +0200

    Fix task 6

--- js/task_6.js
+++ js/task_6.js
@@ -2,13 +2,11 @@ const validationRef = document.querySelector('#validation-input');
 
 validationRef.addEventListener('blur', event => {
   if (event.target.value.length < 6) {
-  return validationRef.classList.add('invalid')}
-  if (event.target.value.length >= 6) 
-  return validationRef.classList.add('valid');
-} 
-//  validationRef.classList.add(event.target.value.length < 6 ? 'invalid' : 'valid')
-)
-
-
-
-
+    validationRef.classList.add('invalid');
+    validationRef.classList.remove('valid');
+  }
+  if (event.target.value.length >= 6) {
+    validationRef.classList.add('valid');
+    validationRef.classList.remove('invalid');
+  }
+});
