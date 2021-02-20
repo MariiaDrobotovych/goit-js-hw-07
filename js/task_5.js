7c22a8e7ed65416cf3b4982c1a23d9ddf965c78f
Maria <maria@phyxologs-MacBook-Pro.local>
2021-02-20 20:23:01 +0200

    fix task 5

--- js/task_5.js
+++ js/task_5.js
@@ -1,6 +1,9 @@
 const inputNameRef = document.querySelector('#name-input')
 const nameOutputRef = document.querySelector('#name-output')
 
-inputNameRef.addEventListener('change', event => {
+inputNameRef.addEventListener('input', event => {
+  if (!event.target.value.length){
+    return nameOutputRef.textContent = 'незнакомец';
+  } 
   nameOutputRef.textContent = event.target.value;
 })
--- js/task_6.js
+++ js/task_6.js
@@ -1,12 +1,14 @@
 const validationRef = document.querySelector('#validation-input');
 
 validationRef.addEventListener('blur', event => {
-  const value = event.target.value;
-  validationRef.classList.add(value.length <= 6 ? 'invalid' : 'valid')
-  // if (value.length <= 6) {
-  //   validationRef.classList.add('invalid')
-  // } else {
-  //   validationRef.classList.add('valid');
-  // } 
-  console.log(value);
-})
+  if (event.target.value.length < 6) {
+  return validationRef.classList.add('invalid')}
+  if (event.target.value.length >= 6) 
+  return validationRef.classList.add('valid');
+} 
+//  validationRef.classList.add(event.target.value.length < 6 ? 'invalid' : 'valid')
+)
+
+
+
+
